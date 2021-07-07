11f5cca5a5a0c9603823498c91e0b65eac363ab4
id <id@IN0013429>
2021-07-07 15:52:45 +0900

    AccountControllerにログインの正否判定機能を追加 UsersRepositoryにfindByEmail()を追加

--- src/main/java/com/example/demo/AccountController.java
+++ src/main/java/com/example/demo/AccountController.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,8 +17,8 @@ public class AccountController {
 	@Autowired
 	HttpSession session;
 
-	//@Autowired
-	//UserRepository userRepository;
+	@Autowired
+	UsersRepository usersRepository;
 
 	/**
 	 * ログイン画面を表示
@@ -30,32 +33,77 @@ public class AccountController {
 	/**
 	 * ログインを実行
 	 */
-	@RequestMapping(value="/login", method=RequestMethod.POST)
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public ModelAndView doLogin(
 			@RequestParam("email") String email,
 			@RequestParam("password") String password,
-			ModelAndView mv
-	) {
+			ModelAndView mv) {
 		// emailが空の場合にエラーとする
-		if(email == null || email.length() == 0) {
+		if (email == null || email.length() == 0) {
 			mv.addObject("message", "メールアドレスを入力してください");
 			mv.setViewName("login");
 			return mv;
 		}
 
 		// passwordが空の場合にエラーとする
-		else if(password == null || password.length() == 0) {
+		else if (password == null || password.length() == 0) {
 			mv.addObject("message", "パスワードを入力してください");
 			mv.setViewName("login");
 			return mv;
+		} else {
+			Users user = null;
+			Optional<Users> record = usersRepository.findByEmail(email);
+
+			if (record.isEmpty() == false) {//アドレスが存在する場合
+
+				user = record.get();
+
+				String truePass = user.getPassword();
+				if (truePass.equals(password)) {
+					// セッションスコープにログイン名とカテゴリ情報を格納する
+					session.setAttribute("userInfo", user);
+					//session.setAttribute("categories", categoryRepository.findAll());
+
+					mv.setViewName("top");
+					return mv;
+				}
+				// 空の場合にエラーとする
+				else {
+					mv.addObject("message", "パスワードが違います");
+					mv.setViewName("login");
+
+				}
+			}else {
+				mv.addObject("message", "メールアドレスが登録されていません");
+				mv.setViewName("login");
+
+			}
 		}
+		return mv;
+	}
 
-		// セッションスコープにログイン名とカテゴリ情報を格納する
-		session.setAttribute("email", email);
-		session.setAttribute("password", password);
-		//session.setAttribute("categories", categoryRepository.findAll());
+	//		// セッションスコープにログイン名とカテゴリ情報を格納する
+	//		session.setAttribute("email", email);
+	//		session.setAttribute("password", password);
+	//		//session.setAttribute("categories", categoryRepository.findAll());
+	//
+	//		mv.setViewName("TOP");
+	//		return mv;
+	//	}
+	/**
+	 * 新規登録画面を表示
+	 */
+	@RequestMapping("/signup")
+	public String signup() {
+		return "signup";
+	}
 
-		mv.setViewName("TOP");
+	@PostMapping("/signup")
+	public ModelAndView doSignup(ModelAndView mv,
+			@RequestParam("name") String name,
+			@RequestParam("email") String mail,
+			@RequestParam("password") String password) {
+
 		return mv;
 	}
 }
--- src/main/java/com/example/demo/UsersRepository.java
+++ src/main/java/com/example/demo/UsersRepository.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+	Optional<Users> findByEmail(String email);
+
+
 }
