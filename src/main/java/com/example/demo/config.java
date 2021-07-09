018dd9347bfec97bccd2d49cd58b7aa5cf4ac9ce
id <id@IN0013429>
2021-07-09 15:12:34 +0900

    TEST01でのNEW表示

--- src/main/java/com/example/demo/config.java
+++ src/main/java/com/example/demo/config.java
@@ -1,11 +1,5 @@
 package com.example.demo;
 
-import java.util.Collections;
-
-import javax.servlet.SessionTrackingMode;
-
-import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,15 +10,5 @@ public class config implements WebMvcConfigurer{
 		 registry.addResourceHandler("/img/**")
 		 .addResourceLocations("file:///C:/pleiades/workspace/id_group03/src/main/resources/templates/img/");
 	 }
-	   @Bean
-	    public ServletContextInitializer servletContextInitializer() {
-	        // 初回アクセス時に、URLにSessionIDが付与されるのを防ぐ
-	        // https://blog.ik.am/entries/353
-	        ServletContextInitializer initializer = servletContext -> {
-	            servletContext.setSessionTrackingModes(
-	                Collections.singleton(SessionTrackingMode.COOKIE)
-	            );
-	        };
-	        return initializer;
-	    }
+
 }
