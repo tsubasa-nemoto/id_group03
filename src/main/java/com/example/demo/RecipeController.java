464ccd57d2a843c4d803c269579fc1257c2341b9
id <id@IN0013429>
2021-07-12 15:51:25 +0900

    検索機能の実装

--- src/main/java/com/example/demo/RecipeController.java
+++ src/main/java/com/example/demo/RecipeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RecipeController {
@@ -24,22 +25,22 @@ HttpSession session;
 	 */
 	@RequestMapping("/search")
 	public ModelAndView searchitems(
-			//@RequestParam("search") String dish,
+			@RequestParam("search") String dish,
 			ModelAndView mv) {
 
 		List<Recipe> recipeList = null;//nullを入れて中身を空っぽに
 		recipeList = recipeRepository.findAll();
-//		if (dish.equals("") ) {//すべて空白だったら
-//			recipeList = recipeRepository.findAll();//全件検索
-//		} else if (!dish.equals("")) {//nameが空白でないとき
-//			recipeList = recipeRepository.findByDishLike("%"+dish+"%");//あいまい検索で表示
-////		} else if (!name.equals("") && !minPrice.equals("") && !maxPrice.equals("")) {//すべて空白でないとき
-////			itemList = itemRepository.findByNameLikeAndPriceBetween("%"+name+"%", Integer.parseInt(minPrice), Integer.parseInt(maxPrice));
-//		}
+		if (dish.equals("") ) {//すべて空白だったら
+			recipeList = recipeRepository.findAll();//全件検索
+			mv.addObject("mes","レシピ一覧");
+			session.setAttribute("mes","レシピ一覧");
+		} else if (!dish.equals("")) {//nameが空白でないとき
+			recipeList = recipeRepository.findByDishLike("%"+dish+"%");//あいまい検索で表示
+		mv.addObject("mes",dish+"の検索結果");
 
 
+		}
 
-		//itemList = itemRepository.findByNameLike("%"+name+"%");//あいまい検索したnameをitemListに格納
 		mv.addObject("recipes",recipeList);//itemListをitems(list.htmlのth:eachのところ)に格納
 		//mv.addObject("search", dish);//テキストボックスに保持(menu.htmlのth:valueで格納するキーをつくる)
 		session.setAttribute("recipes", recipeList);
